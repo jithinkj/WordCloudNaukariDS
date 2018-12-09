ed505bf5ae2f47a7b3153a3a3503c701b4f1d5ca
jithin <jithinkj13@gmail.com>
2018-12-09 10:51:02 +0530

    test1

--- WebScraping/__init__.py
+++ WebScraping/__init__.py
@@ -1,1 +1,31 @@
-#Beautiful soup is using to scarp details from naukari website
+# Beautiful soup is using to scarp details from naukari website
+import bs4
+# beautiful-soup imported
+import requests
+# requests imported
+title = []
+summary = []
+location = []
+company = []
+experienceList = []
+
+res = requests.get('https://www.indeed.co.in/jobs?q=data+science&l=India')
+# print(res.text)
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(soup)
+for t in soup.select(".jobtitle.turnstileLink"):
+    title.append(t.text.lstrip())
+for s in soup.select(".summary"):
+    summary.append(s.text.lstrip())
+for l in soup.select(".location"):
+    location.append(l.text.lstrip())
+for c in soup.select(".company"):
+    company.append(c.text.lstrip())
+# for e in soup.select(".experienceList"):
+#     experienceList.append(e.text.lstrip())
+print(title[3])
+print(summary[3])
+print(location[3])
+print(company[3])
+# print(experienceList[3])
+
